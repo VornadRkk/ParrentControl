ca2e39beff0af04cd2d7b8c8781476e301d480f8
VornadRkk <126707014+VornadRkk@users.noreply.github.com>
2024-12-03 15:25:56 +0400

    start version

---
+++ app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+import functional
+
+
+
+root = tk.Tk()  # Создаёт главное окно
+root.title("Parrent control")  # Устанавливает заголовок окна
+root.geometry("500x600")  # Устанавливает размеры окна (ширина x высота)
+root.mainloop()  # Запускает основной цикл приложения
